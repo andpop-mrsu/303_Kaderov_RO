821d42c927fdb29909cefc40d6f712d81ee4c7ec
kaderovro <rk.kaderov|@mail.ru>
2022-09-24 16:43:16 +0300

    Task02

---
+++ Task02/make_db_init.py
@@ -0,0 +1,31 @@
+import sqlite3
+conn = sqlite3.connect('movies_rating.db')
+cursorObj = conn.cursor()
+cursorObj.execute("""CREATE TABLE movies (
+                        id INTEGER PRIMARY KEY,
+                        title VARCHAR(255) NOT NULL,
+                        year INTEGER NOT NULL,
+                        genres VARCHAR(255) NOT NULL)""")
+
+cursorObj.execute("""CREATE TABLE ratings (
+                        id INTEGER PRIMARY KEY,
+                        user_id INTEGER NOT NULL,
+                        movie_id INTEGER NOT NULL,
+                        rating INTEGER,
+                        timestamp)""")
+
+cursorObj.execute("""CREATE TABLE tags (
+                        id INTEGER PRIMARY KEY,
+                        user_id INTEGER NOT NULL,
+                        movie_id INTEGER NOT NULL,
+                        tag VARCHAR(255) NOT NULL,
+                        timestamp)""")
+
+cursorObj.execute("""CREATE TABLE users (
+                        id INTEGER PRIMARY KEY,
+                        name VARCHAR(255) NOT NULL,
+                        email VARCHAR(255) NOT NULL,
+                        gender VARCHAR(255) NOT NULL,
+                        register_date DATE NOT NULL,
+                        occupation VARCHAR(255) NOT NULL)""")
+conn.commit()
