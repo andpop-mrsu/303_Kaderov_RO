d4e9edbf2064142f6418700802b4136faddddc1d
RKSvetoFoR <98745161+RKSvetoFoR@users.noreply.github.com>
2022-12-15 22:08:47 +0300

    Update data_add.sql

--- Task05/data_add.sql
+++ Task05/data_add.sql
@@ -2,7 +2,7 @@ INSERT INTO users (first_name,last_name,email,gender,occupation_id) VALUES ("Pav
 INSERT INTO users (first_name,last_name,email,gender,occupation_id) VALUES ("Dmitry","Katin","katin.dmitry@icloud.com","male",19);
 INSERT INTO users (first_name,last_name,email,gender,occupation_id) VALUES ("Maxim","Negrya","maxi_n@gmai.com","male",19);
 INSERT INTO users (first_name,last_name,email,gender,occupation_id) VALUES ("Egor","Melyakin","egor_m@outlook.com","male",19);
-INSERT INTO users (first_name,last_name,email,gender,occupation_id) VALUES ("Margar","Melkonyan","margar.melkonyan@bk.ru","male",19);
+INSERT INTO users (first_name,last_name,email,gender,occupation_id) VALUES ("Roman","Kaderov","roman.kaderov@bk.ru","male",19);
 
 
 INSERT INTO movies(title,year) VALUES
@@ -11,9 +11,9 @@ INSERT INTO movies(title,year) VALUES
 ('Stranger things 4',2022);
 
 INSERT INTO ratings(user_id,movie_id,rating) VALUES
-((SELECT id FROM users WHERE email="margar.melkonyan@bk.ru" ),
+((SELECT id FROM users WHERE email="roman.kaderov@bk.ru" ),
 (SELECT id FROM movies WHERE title="Chainsaw Man"),      4.5),
-((SELECT id FROM users WHERE email="margar.melkonyan@bk.ru" ),
+((SELECT id FROM users WHERE email="roman.kaderov@bk.ru" ),
 (SELECT id FROM movies WHERE title="Mob Psycho 100 III"),3.5),
-((SELECT id FROM users WHERE email="margar.melkonyan@bk.ru" ),
+((SELECT id FROM users WHERE email="roman.kaderov@bk.ru" ),
 (SELECT id FROM movies WHERE title="Stranger things 4"), 5.0);
